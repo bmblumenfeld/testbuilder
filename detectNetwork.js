a011aab3b27ddcf1c1a35818f7bd684c5d788007
Ben Blumenfeld <bmblumenfeld@gmail.com>
2017-11-29 17:08:18 -0800

    Added regex instead of conditional strings

--- detectNetwork.js
+++ detectNetwork.js
@@ -7,17 +7,19 @@
 //   1. The first few numbers (called the prefix)
 //   2. The number of digits in the number (called the length)
 
-var detectNetwork = function(cardNumber) {
-  // Note: `cardNumber` will always be a string
-  // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
-  var prefix = Number((cardNumber[0] + cardNumber[1]))
-  var cardNetwork = null 
-  if (((prefix === 38) || (prefix === 39)) && (cardNumber.length === 14)) {
-    cardNetwork = 'Diner' + '\'' + 's' + ' Club'
-  } else if (((prefix === 34) || (prefix === 37)) && (cardNumber.length === 15)) {
-      cardNetwork = 'American Express'
-    } else if 
-  // The American Express network always starts with a 34 or 37 and is 15 digits long
-  return cardNetwork 
-};
 
+var detectNetwork = function(cardNumber) {
+  var network = null
+  var cardNetworks = {
+    "Diner\'s Club" : cardNumber.match(/^(3[8|9][0-9]{12})$/),
+    "American Express" : cardNumber.match(/^(3[4|7][0-9]{13})$/),
+  }
+
+  for (card in cardNetworks) {
+    if (cardNetworks[card] !== null) {
+      network = card
+    }
+  }
+  return network
+  console.log()
+};
